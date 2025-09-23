6c9025b32fe112a6bdd23c8cde5303615fc40ffa
Ankit Singh <142376440+chauhanAni5959@users.noreply.github.com>
2025-09-23 23:36:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your code here
+// Bootstrap validation
+    (() => {
+      'use strict'
+      const forms = document.querySelectorAll('.needs-validation')
+      Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+          form.classList.add('was-validated')
+        }, false)
+      })
+    })()
+  </script>
